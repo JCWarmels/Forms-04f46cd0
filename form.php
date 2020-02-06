2db26ed480fdf5592c5c93ab21536d7ad80b2765
JCWarmels <jesse.warmels@student.rocva.nl>
2020-02-06 14:31:04 +0100

    Uitwerking van de exercise

---
+++ form.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+    <form>
+        <input type="text" name="emailin" placeholder="Voer hier je emailadress in."/>
+        <input type="submit" name="submit" value="submit"/>
+    </form>
+</body>
+</html>
